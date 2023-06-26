9023b214bedb55edf3b10d080275068e4373e19a
Mak Poling <makpwebdev@gmail.com>
2023-06-26 00:27:22 -0400

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const toggler = document.getElementById('toggler');
-const nav = document.getElementById('navbar');
-
-toggler.addEventListener('click', () => {
-  nav.classList.toggle('open');
-});
