1c9cc59ef1685fcc9f4279919888804118e9c16b
Mak Poling <makpwebdev@gmail.com>
2023-06-26 00:23:24 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const toggler = document.getElementById('toggler');
+const nav = document.getElementById('navbar');
+
+toggler.addEventListener('click', () => {
+  nav.classList.toggle('open');
+});
